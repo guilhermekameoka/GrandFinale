8d97393bd5882e631eeb035b2ad7fc7a05b25561
CarlosLivius <carlinhoslivius123@gmail.com>
2023-08-20 19:14:05 -0300

    feat: Front novin em foia

---
+++ src/front-end/static/js/functions.js
@@ -0,0 +1,14 @@
+const textoEmImagens = document.getElementById('op1');
+const response = document.getElementById('response');
+
+function visibility() {
+    console.log('visibility');
+
+    if (response.style.display == 'none') {
+        response.style.display = 'block';
+    } else{
+        response.style.display = 'block';
+    }
+
+    
+}
